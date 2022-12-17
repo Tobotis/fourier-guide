bbc4fc28b1ccec83b385abfc0252bf1feb96e5d5
Tobotis <to.steinbrecher@gmail.com>
2022-12-17 23:23:35 +0100

    removed legacy keyword and added search option

--- theme.config.jsx
+++ theme.config.jsx
@@ -24,7 +24,9 @@ export default {
     </>
   ),
   //primaryHue: 299,
-  search: false,
+  search: {
+    placeholder: 'Suchen...',
+  },
   navigation: true,
   footer: true,
   titleSuffix: false,
