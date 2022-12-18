626693abc5432da8ffd377e8549615590a8ce3d9
Tobotis <to.steinbrecher@gmail.com>
2022-12-18 11:47:35 +0100

    Added content

--- components/FrameText.tsx
+++ components/FrameText.tsx
@@ -10,7 +10,7 @@ const FrameText = ({ display, text }: FrameTextProps) => {
   return (
     <div className="my-4 flex flex-row gap-5">
       <div className="rounded-lg overflow-hidden flex-none">{display}</div>
-      <div className="shrink hover:font-medium transition-all">{text}</div>
+      <div className="shrink">{text}</div>
     </div>
   )
 }
