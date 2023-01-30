50cbda8eda9fd645228bab40dc3e620f0575f83d
Tobotis <to.steinbrecher@gmail.com>
2023-01-30 22:10:24 +0100

    added vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,5 +2,10 @@ import '../globals.css'
 import 'nextra-theme-docs/style.css'
 
 export default function Nextra({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  )
 }
